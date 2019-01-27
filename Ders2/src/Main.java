5fff3b123c0010c54f53ff5f7eaa75e533b2c108
yagmur.kilic <yagmur-k-l-c-91@windowslive.com>
2019-01-27 14:14:11 +0300

    Tip dönüşümü

---
+++ Ders2/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String [] args) {
+
+        int a=5;
+        System.out.println("Bu bir int değerdir :"+a);
+
+        Long b =300000000000L;
+        System.out.println("Bu bir long değerdir :"+b);
+
+        Integer c =8;
+        System.out.println("Bu bir integer değerdir :"+c);
+        long x =a+b+c;
+        System.out.println("toplam (a+b+c)  :"+x);
+        Integer y =new Integer (8);
+        System.out.println("Bu bir integer değerdir :"+y);
+
+
+
+       /* String b ="Yağmur";*/
+
+
+
+    }
+}
---
+++ Ders2/src/tipDonusumleri/Main.java
@@ -0,0 +1,22 @@
+package tipDonusumleri;
+
+public class Main {
+
+    public static void main (String [] args) {
+
+        byte a =127;
+        int b;
+        b=a;
+        b=1000;
+        a=(byte) b;
+        System.out.println(a);
+
+
+       Integer xInt=12;
+       String xString;
+       xString=xInt.toString();
+        System.out.println(xString);
+
+
+    }
+}
