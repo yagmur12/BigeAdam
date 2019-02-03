21a8afbf06d6428fb956ee84174d0dd7c34abd4a
yagmur.kilic <yagmur-k-l-c-91@windowslive.com>
2019-02-03 10:46:38 +0300

    klavyeden girien iki sayının toplamı

---
+++ Ders4/src/klavyeGirdileri/KlavyeGirdiApp.java
@@ -0,0 +1,21 @@
+package klavyeGirdileri;
+import java.util.*;
+public class KlavyeGirdiApp {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+
+        //scanner.nextInt();
+       // scanner.nex5tDouble();
+       //scanner.next(); string değer okumak için
+       //scanner.nextLine();String tüm değerleri okusun diye satırın boyutu kadar.
+        int sayi1,sayi2,sonuc;
+        System.out.print("Bir sayı giriniz:");
+        sayi1=scanner.nextInt();
+        System.out.println("Girdiğiniz sayi:"+sayi1);
+        System.out.print("İkinci sayıyı giriniz:");
+        sayi2=scanner.nextInt();
+        System.out.println("Girdiğiniz ikinci sayi:"+sayi2);
+        sonuc=sayi1+sayi2;
+        System.out.println("Toplam sonuç:"+sonuc);
+    }
+}
